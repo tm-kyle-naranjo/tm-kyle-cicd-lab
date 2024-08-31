49ea11c43939b486c07d3312688f0bb2e268b0ad
tm-kyle-naranjo <kyle.n@thinkingmachin.es>
2024-08-31 22:36:23 +0800

    fix: set params to floats

--- print_script.py
+++ print_script.py
@@ -4,14 +4,15 @@ import sys
 def main():
     """Main function to run the script."""
     try:
-        num_a, num_b = int(sys.argv[1]), int(sys.argv[2])
+        num_a = float(sys.argv[1])
+        num_b = float(sys.argv[2])
     except (ValueError, IndexError):
-        print("Error: Two integer arguments are required.")
+        print("Error: Two numeric arguments are required.")
         sys.exit(1)
     
     print(f"num_a: {num_a}")
     print(f"num_b: {num_b}")
-    print(f"Sum: {num_a + num_b}")
+    print(f"sum: {num_a + num_b}")
 
 
 if __name__ == "__main__":
